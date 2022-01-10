9004b916ff7e6bf0cd90518f74d238dd0912cfc2
luiscapello <capello3005@gmail.com>
2022-01-10 09:25:57 -0600

    Update main.py
    update main

---
+++ 20-proyecto-python/main.py
@@ -0,0 +1,14 @@
+
+print("""
+Acciones disponibles
+    -Registro
+    -Ingresar
+""")
+
+acciones = input("Que Accion deseas realizar? ")
+
+if accion == "Registro":
+    print("Ok!! Vamos a registrarte en el sistema ...")
+
+elif accion == "Ingresar":
+    print(" Vale!! vamos a ingresar en el sistema ...")
