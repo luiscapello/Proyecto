59a51bb763056523346fc788c762aabfc815e667
luiscapello <capello3005@gmail.com>
2022-01-12 12:48:31 -0600

    Create acciones.py

---
+++ 20-proyecto-python/usuarios/acciones.py
@@ -0,0 +1,17 @@
+
+class Acciones:
+
+    def registro(self):
+        print("\nOk!! Vamos a registrate en el sistema...")
+
+        nombre = input("¿Cual es tu nombre? ")
+        apellidos = input("¿Cuales son tus apellidos? ")
+        email = input("Introduce tu email: ")
+        password = input("Introduce tu contraseña: ")
+
+    def ingresar(self):
+        print("Vale!! identificate en el sistema")
+
+        email = input("Introduce tu email: ")
+        password = input("Introduce tu contraseña:")
+        
