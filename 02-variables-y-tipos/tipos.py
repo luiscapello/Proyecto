50fac77b04367cc8367e4c03e2473c4be7988ff2
luiscapello <capello3005@gmail.com>
2022-01-12 21:37:08 -0600

    Update tipos.py

--- 02-variables-y-tipos/tipos.py
+++ 02-variables-y-tipos/tipos.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 nada = None
 cadena = "hola"
 entero = 99
@@ -14,4 +15,22 @@ print(diccionario)
 
 #mostrar tipo de dato
 
+=======
+nada = None
+cadena = "hola"
+entero = 99
+booleano = True
+lista = [10, 20, 30, 40]
+tupla = ("master", "en")
+diccionario = {
+    "nombre": "Luis",
+    "Apellido": "Capello",
+    "Curso": "python"
+}
+
+print(diccionario)
+
+#mostrar tipo de dato
+
+>>>>>>> Stashed changes
 print(type(diccionario))
