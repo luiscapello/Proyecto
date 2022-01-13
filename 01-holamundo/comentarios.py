0ff5a932c6f97bbf6f4646610dc0efcc4809df06
luiscapello <capello3005@gmail.com>
2022-01-12 21:36:58 -0600

    Update comentarios.py

--- 01-holamundo/comentarios.py
+++ 01-holamundo/comentarios.py
@@ -1,5 +1,12 @@
+<<<<<<< Updated upstream
 # esto me mostrara el texto por pantalla
 print("Hola")
 #estas dos muestras texto por pantalla
 print("Mundo")
+=======
+# esto me mostrara el texto por pantalla
+print("Hola")
+#estas dos muestras texto por pantalla
+print("Mundo")
+>>>>>>> Stashed changes
 print("!!")
