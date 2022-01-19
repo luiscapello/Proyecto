f5be516a9cf813649dc0e2ce0a97b89213c9b63f
luiscapello <capello3005@gmail.com>
2022-01-19 17:05:23 -0600

    Update 01-ventana.py

--- 21-tkinter/01-ventana.py
+++ 21-tkinter/01-ventana.py
@@ -1,4 +1,6 @@
+from cProfile import label
 from tkinter import *
+import os.path
 
 #crear la ventana raiz
 ventana = Tk()
@@ -6,14 +8,26 @@ ventana = Tk()
 #titulo e la ventana
 ventana.title("Login uyeda")
 
+#comprobar si existe un archivo
+ruta_icono = os.path.abspath('./img/rocket-ship.ico')
+
+#Icono de la ventana
+#ventana.iconbitmap(ruta_icono)
+
+if not os.path.isfile(ruta_icono):
+    ruta_icono = os.path.abspath('./21-tkinter/img/rocket-ship.ico')
+
+#mostrar texto en el programa
+texto = Label(ventana, text=ruta_icono)
+texto.pack()
+
 #cambio en el tamaño de la ventana
-ventana.geometry("750x500")
+ventana.geometry("650x500")
 
 #bloquear el tamaño de la ventana
 ventana.resizable(0,0)
 
-#Icono de la ventana
-ventana.iconbitmap("./21-tkinter/img/rocket-ship.ico")
+
 
 #arrancar y mostrar la ventana hasta que se cierre
 ventana.mainloop()
