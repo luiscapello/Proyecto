27b38af1a37fc1184694a1e9c10f13f85a485531
luiscapello <capello3005@gmail.com>
2022-01-25 15:24:55 -0600

    Update 06-formularios.py

--- 21-tkinter/06-formularios.py
+++ 21-tkinter/06-formularios.py
@@ -19,7 +19,7 @@ encabezado.grid(row=0, column=0, columnspan=12, sticky=W)
 
 #label para e campo (nombre)
 label= Label(ventana, text="Nombre")
-label.grid(row=1, column=0,sticky=W, padx=5, pady=5)
+label.grid(row=1, column=0, padx=5, pady=5)
 
 # Campo de texto (nombre)
 campo_texto = Entry(ventana)
@@ -28,11 +28,23 @@ campo_texto.config(justify="right", state="normal")
 
 #label para e campo (apellido)
 label= Label(ventana, text="Apellidos")
-label.grid(row=2, column=0, sticky=W, padx=5, pady=5)
+label.grid(row=2, column=0, padx=5, pady=5)
 
 # Campo de texto (apellidos)
 campo_texto = Entry(ventana)
-campo_texto.grid(row=2, column=1,sticky=W, padx=5, pady=5)
+campo_texto.grid(row=2, column=1, sticky=W, padx=5, pady=5)
 campo_texto.config(justify="left", state="normal")
+
+#label para e campo (descripcion)
+label = Label(ventana, text="Descripción")
+label.grid(row=3, column=0, padx=5, pady=5)
+
+#campo de texto grande(descripcion)
+campo_grande = Text(ventana)
+campo_grande.grid(row=3, column=1)
+campo_grande.config(
+    width=30,
+    height=5
+)
 
 ventana.mainloop()
