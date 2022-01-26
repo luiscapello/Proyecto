99abedb75e9efed0e916007c6866f10d7ea293c9
luiscapello <capello3005@gmail.com>
2022-01-26 11:41:39 -0600

    Update 08-alertas.py
    calculadora

--- 21-tkinter/08-alertas.py
+++ 21-tkinter/08-alertas.py
@@ -1,3 +1,4 @@
+from site import venv
 from tkinter import *
 from tkinter import messagebox as Messagebox
 
@@ -8,5 +9,15 @@ def sacarAlerta():
     Messagebox.showinfo("Alerta", "Hola soy Capello")
 
 Button(ventana, text="Mostrar alerta!! ", command = sacarAlerta).pack()
+
+def salir(nombre):
+    resultado = Messagebox.askquestion("Salir", "Continuar ejecutando la aplicación?")
+
+    if resultado != "yes":
+        Messagebox.showinfo("Adios!! ", f"Adios {nombre}")
+        ventana.destroy()
+
+
+Button(ventana, text="Salir!! ", command =lambda: salir("capello")).pack()
 
 ventana.mainloop()
