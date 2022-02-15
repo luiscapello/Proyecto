3ae4163e58e59dacaa2f04ef73515e0d2b3c0637
luiscapello <capello3005@gmail.com>
2022-02-15 17:34:29 -0600

    connect-mysql

--- 22-django/ProyectoDjango/ProyectoDjango/settings.py
+++ 22-django/ProyectoDjango/ProyectoDjango/settings.py
@@ -77,11 +77,22 @@ WSGI_APPLICATION = 'ProyectoDjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyectodjango',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
