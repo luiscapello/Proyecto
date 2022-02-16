8a140f63e6b3bb94bf6d4ab160b595be9a20a0cb
luiscapello <capello3005@gmail.com>
2022-02-16 17:36:19 -0600

    flask

--- 23-flask/main.py
+++ 23-flask/main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return"Aprendiendo"
 
+@app.route('/informacion')
+def informacion():
+    return "<h1>Pagina de información</h1>"
+
+@app.route('/contacto')
+def contacto():
+    return "<h1>Pagina de contacto</h1>"
+
+@app.route('/lenguajes-de-programacion')
+def lenguajes():
+    return "<h1>Pagina de lenguajes</h1>"
+
 if __name__ == '__main__':
     app.run(debug=True)
